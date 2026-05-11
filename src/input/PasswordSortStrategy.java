a5975b7fdbb7d7eb54606989914e23e4a0193dcb
Roman Emelyanov <rrrw2@mail.ru>
2026-05-11 13:53:07 +0300

    Сортировка по длине пароля

---
+++ src/input/PasswordSortStrategy.java
@@ -0,0 +1,10 @@
+package input;
+import sorting.SortingStrategy;
+import java.util.Comparator;
+import java.util.List;
+
+public class PasswordSortStrategy implements SortingStrategy {
+    public void sort(List<User> users) {
+        users.sort(Comparator.comparing(user -> user.getPasswordLength()));
+    }
+}
